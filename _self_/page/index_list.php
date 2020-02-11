56fda57dbe5b019c1708b30489213f0ef4ca75b1
glenn <glenn.profile@gmail.com>
2020-02-11 10:47:33 +0800

    feat: server_status_processes quick link 包含 query string 的內容, 至少需要 server 這個參數

--- _self_/page/index_list.php
+++ _self_/page/index_list.php
@@ -7,11 +7,17 @@ code
     include '_self_/page/index_list.php';
 
 */
+
+$queryString = '';
+if (isset($_SERVER['QUERY_STRING'])) {
+    $queryString = '?' . $_SERVER['QUERY_STRING'];
+}
+
 ?>
 
 <code>
 <h2>常用連結</h2>
 <ul>
-    <li><a href="server_status_processes.php">status &gt; processes</a></li>
+    <li><a href="server_status_processes.php<?php echo $queryString; ?>">status &gt; processes</a></li>
 </ul>
 </code>
